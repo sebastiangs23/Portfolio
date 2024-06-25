39480a50928f6fa3c7c1183c323c93f94c6af793
sebastiangs23 <sebastiangs2309@gmail.com>
2024-06-24 21:09:58 -0500

    refactor(Refactor): :fire: adjust the height

--- front/portfolio/src/components/Tecnologies/Tecnologies.jsx
+++ front/portfolio/src/components/Tecnologies/Tecnologies.jsx
@@ -11,6 +11,7 @@ import redux from "../../assets/tecnologies-black/client/reduxb.png";
 
 /*_____________
 |   SERVER   */
+import typescript from "../../assets/tecnologies-black/server/typescript.png";
 import node from "../../assets/tecnologies-black/server/node.png";
 import express from "../../assets/tecnologies-black/server/expressssb.png";
 
@@ -30,73 +31,80 @@ import sequelize from "../../assets/tecnologies-black/orm/sequelize.png";
 import aws from "../../assets/tecnologies-black/cloud/aws.webp";
 import github from "../../assets/tecnologies-black/cloud/github.png";
 
-
-
 function Tecnologies() {
   return (
     <div className="container-tecnologies" id="tecnologias">
       <h2 className="section__title">Tecnologías</h2>
       <div className="flex-container">
+        {/* FRONT */}
         <div className="seccion">
           <h3 className="subtitulo">Front-end</h3>
           <div className="solo">
-            <img src={react} alt="" className="imagen" id="reactjs" />
+            <img src={react} className="imagen" id="reactjs" />
           </div>
           <div className="solo">
-            <img src={redux} alt="" className="imagen" />
+            <img src={redux} className="imagen" />
           </div>
           <div className="solo">
-            <img src={reactNative} alt="" className="imagen" />
+            <img src={reactNative} className="image-big" />
           </div>
           <div className="solo">
-            <img src={figma} alt="" />
+            <img src={figma} className="image-big" />
           </div>
           <div className="solo">
-            <img src={vue} alt="" />
+            <img src={vue} className="image-big" />
           </div>
         </div>
+
+        {/* SERVER */}
         <div className="seccion">
-          <h3 className="subtitulo">Server</h3>
+          <h3 className="subtitulo">Back-end</h3>
           <div className="solo">
-            <img src={node} alt="" className="imagen" id="nodeimg" />
+            <img src={typescript} className="image-big" />
           </div>
           <div className="solo">
-            <img src={express} alt="" className="imagen" id="express" />
+            <img src={node} className="imagen" id="nodeimg" />
+          </div>
+          <div className="solo">
+            <img src={express} className="imagen" id="express" />
           </div>
         </div>
+
+        {/* DATABASE */}
         <div className="seccion">
-          <h3 className="subtitulo">Database</h3>
+          <h3 className="subtitulo">Base de datos</h3>
           <div className="solo">
-            <img src={mysql} alt="" className="imagen" />
+            <img src={mysql} className="imagen" />
           </div>
           <div className="solo">
-            <img src={postgres} alt="" className="imagen" />
+            <img src={postgres} className="image-big" />
           </div>
           <div className="solo">
-            <img src={mongodb} alt="" className="imagen" />
+            <img src={mongodb} className="image-mongo" />
           </div>
         </div>
+
+        {/* ORM */}
         <div className="seccion">
           <h3 className="subtitulo">ORM</h3>
           <div className="solo">
-            <img src={sequelize} alt="" className="imagen" />
+            <img src={sequelize} className="image-big" />
           </div>
           <div className="solo">
-            <img src={mongoose} alt="" className="imagen" />
+            <img src={mongoose} className="imagen" />
           </div>
         </div>
 
+        {/* CLOUD, HOSTING & VERSION CONTROL */}
         <div className="seccion">
           <h3 className="subtitulo">Cloud, hosting & Version Control</h3>
           <div className="solo">
-            <img src={aws} alt="" className="imagen" />
+            <img src={aws} className="image-big" />
           </div>
           <div className="solo">
-            <img src={github} alt="" className="imagen" />
+            <img src={github} className="image-big" />
           </div>
-          <div>
-
-          </div>
+          <div></div>
         </div>
       </div>
     </div>
