b40b01b3f94efab5606546ec006dffe6e041b1e4
sebastiangs23 <sebastiangs2309@gmail.com>
2026-05-13 11:13:56 +0400

    feat: animation on the job title

--- front/portfolio/src/components/Home/Home.jsx
+++ front/portfolio/src/components/Home/Home.jsx
@@ -13,27 +13,26 @@ export default function Home() {
         <div className="home__content grid">
           <Social />
           <div className="home__img-container">
-          <div className="home__img" onClick={() => setShowHand(true)}>
-            {showHand && (
-              <motion.div
-                initial={{ y: 100, opacity: 0, rotate: -30 }}
-                animate={{ y: -20, opacity: 1, rotate: 0 }}
-                exit={{ y: 100, opacity: 0 }}
-                transition={{ type: "spring", stiffness: 200 }}
-                className="fixed inset-0 flex items-center justify-center text-[13vw] z-50"
-                onAnimationComplete={() =>
-                  setTimeout(() => setShowHand(false), 1000)
-                }
-              >
-                👨🏻‍💻
-              </motion.div>
-            )}
-          </div>
-
+            <div className="home__img" onClick={() => setShowHand(true)}>
+              {showHand && (
+                <motion.div
+                  initial={{ y: 100, opacity: 0, rotate: -30 }}
+                  animate={{ y: -20, opacity: 1, rotate: 0 }}
+                  exit={{ y: 100, opacity: 0 }}
+                  transition={{ type: "spring", stiffness: 200 }}
+                  className="fixed inset-0 flex items-center justify-center text-[13vw] z-50"
+                  onAnimationComplete={() =>
+                    setTimeout(() => setShowHand(false), 1000)
+                  }
+                >
+                  👨🏻‍💻
+                </motion.div>
+              )}
+            </div>
           </div>
           <Data />
         </div>
       </div>
     </section>
   );
-};
+}
